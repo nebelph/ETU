a6834554d9ce3fdbf8eb01e69921a10bda488d23
nebelph <114959269+nebelph@users.noreply.github.com>
2022-10-04 02:02:31 +0300

    Update lab1.cpp

--- lab1.cpp
+++ lab1.cpp
@@ -2,89 +2,66 @@
 using namespace std;
 int main()
 {
-    setlocale(0, "");
-    int taskNumber;
-    cout << "Enter the task number: ";
-    cin >> taskNumber;
+    cout << "\t\tTask 1\n";
+    cout << "int: " << sizeof(int) << "\n";
+    cout << "short int: " << sizeof(short int) << "\n";
+    cout << "long int: " << sizeof(long int) << "\n";
+    cout << "float: " << sizeof(float) << "\n";
+    cout << "double: " << sizeof(double) << "\n";
+    cout << "long double: " << sizeof(long double) << "\n";
+    cout << "char: " << sizeof(char) << "\n";
+    cout << "bool: " << sizeof(bool);
 
-    switch (taskNumber)
+    cout << "\n\n\t\tTask 2\n";
+    cout << "Enter int: ";
+    int input;
+    cin >> input;
+    int a = sizeof(int) * 8;
+    int mask = 1 << a - 1;
+    for (int i = 1; i <= a; i++)
     {
-    case 1:
-        // task 1
+        cout << ((input & mask) ? 1 : 0);
+        if (i == 1 || i % 8 == 0)
+            cout << " ";
 
-        cout << "\nint: " << sizeof(int) << "\n";
-        cout << "short int: " << sizeof(short int) << "\n";
-        cout << "long int: " << sizeof(long int) << "\n";
-        cout << "float: " << sizeof(float) << "\n";
-        cout << "double: " << sizeof(double) << "\n";
-        cout << "long double: " << sizeof(long double) << "\n";
-        cout << "char: " << sizeof(char) << "\n";
-        cout << "bool: " << sizeof(bool) << "\n\n";
-        break;
+        input <<= 1;
+    }
 
-    case 2:
-        // task 2
+    cout << "\n\n\t\tTask 3\n";
+    union {
+        int tool;
+        float f;
+    };
+    cout << "Enter float: ";
+    cin >> f;
+    for (int i = 1; i <= a; i++)
     {
-        cout << "\nEnter int: ";
-        int input;
-        cin >> input;
-        int a = sizeof(int) * 8;
-        int mask = 1 << a - 1;
-        for (int i = 1; i <= a; i++)
-        {
-            cout << ((input & mask) ? 1 : 0);
-            if (i == 1 || i % 8 == 0)
-                cout << " ";
+        cout << ((tool & mask) ? 1 : 0);
+        if (i == 1 || i % 8 == 0)
+            cout << " ";
+        tool <<= 1;
+    }
 
-            input <<= 1;
-        } 
-        break;
-    }
-    case 3:
-        // task 3
+    cout << "\n\n\t\tTask 4\n";
+    union {
+        double d;
+        int arr[2];
+    };
+    cout << "Enter double: ";
+    cin >> d;
+    for (int i = 1; i <= a; i++)
     {
-        union {
-            int tool;
-            float input;
-        };
-        cout << "\nEnter float: ";
-        cin >> input;
-        int a = sizeof(int) * 8;
-        int mask = 1 << a - 1;
-        for (int i = 1; i <= a; i++)
-        {
-            cout << ((tool & mask) ? 1 : 0);
-            if (i == 1 || i % 8 == 0)
-                cout << " ";
-            tool <<= 1;
-        }
-        break;
+        cout << ((arr[1] & mask) ? 1 : 0);
+        if (i == 1 || i % 8 == 0)
+            cout << " ";
+        arr[1] <<= 1;
     }
-    case 4:
-        // task 4
+    for (int i = 1; i <= a; i++)
     {
-        union {
-            double input;
-            int arr[2];
-        };
-        cout << "\nEnter double: ";
-        cin >> input;
-        int a = sizeof(int) * 8;
-        int mask = 1 << a - 1;
-        for (int i = 1; i <= a; i++)
-        {
-            cout << ((arr[1] & mask) ? 1 : 0);
-            if (i == 1 || i % 8 == 0)
-                cout << " ";
-            arr[1] <<= 1;
-        }
-        for (int i = 1; i <= a; i++)
-        {
-            cout << ((arr[0] & mask) ? 1 : 0);
-            if (i % 8 == 0)
-                cout << " ";
-            arr[0] <<= 1;
-        }
+        cout << ((arr[0] & mask) ? 1 : 0);
+        if (i % 8 == 0)
+            cout << " ";
+        arr[0] <<= 1;
     }
-    }
+    cout << "\n\n";
 }
